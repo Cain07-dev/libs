ea56f628b36a32cb809c953e8de861fdacbb5f60
cain07-dev <cain981116@gmail.com>
2024-04-16 06:44:20 +0000

    add web.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
 
 setuptools.setup (
-    name = "",
-    packages = [""],
+    name = "libs_zip",
+    packages = ["libs"],
+    version = "0.0.0",
     download_url = "",
     classifiers=[
         "Programming Language :: Python :: 3"],
+    install_requires=[
+        'selenium',
+        'beautifulsoup4',
+    ],
 )
